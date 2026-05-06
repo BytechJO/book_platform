ec9c349694c44fb112ac5eb54649707594382943
waleed-alqadi <developer3@bytechjo.com>
2026-05-06 15:26:30 +0300

    student done

--- frontend/src/pages/admin/Dashboard/reports/Reports.jsx
+++ frontend/src/pages/admin/Dashboard/reports/Reports.jsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import ActivivtyReport from "./activivtyReport";
+import ActivivtyReport from "./ActivivtyReport";
 export default function Reports() {
 
   return (
